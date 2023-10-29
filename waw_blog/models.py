2de17906430a8d1a3442c70b3af9942127dd07ca
eric-blake <>
2023-10-29 17:38:24 +0000

    Set up admin panel

--- waw_blog/models.py
+++ waw_blog/models.py
@@ -4,9 +4,9 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0,"Draft"), (1, "Published"))
 
-class POST(models.Model):
-    title = models.CharField(max_length =200, unique = True)
-    slug = models.SlugField(max_length =200, unique = True)
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique = True)
+    slug = models.SlugField(max_length=200, unique = True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     created_on = models.DateTimeField(auto_now_add = True)
     content = models.TextField()
