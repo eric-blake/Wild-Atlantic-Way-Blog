aa8f657fb834d5425df83ad30e4358547f430571
eric-blake <>
2023-10-30 11:15:11 +0000

    Add comment model and admin panel functionality

--- waw_blog/models.py
+++ waw_blog/models.py
@@ -2,24 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0,"Draft"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique = True)
-    slug = models.SlugField(max_length=200, unique = True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    created_on = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default = 'placeholder')
+    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
     view_count = models.IntegerField(default=0)
     comment_count = models.IntegerField(default=0)
-    
 
     class Meta:
-        ordering= ['-created_on']
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
@@ -28,5 +29,17 @@ class Post(models.Model):
         return self.likes.count()
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ['created_on']
 
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
