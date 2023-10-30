542f44bd7499089eaf766112f1e86ec3c4247878
eric-blake <>
2023-10-30 13:41:27 +0000

    Add postlist view to views.py

--- waw_blog/views.py
+++ waw_blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Publisher.objects.all()
+    template_name = "index.html"
+
+
+
+
