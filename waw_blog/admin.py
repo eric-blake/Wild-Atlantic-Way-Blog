b6c9b9c6e98d741d6fd996fbfa207b8b628ad8da
eric-blake <>
2023-10-30 10:40:45 +0000

    Update admin panels for posts

--- waw_blog/admin.py
+++ waw_blog/admin.py
@@ -3,10 +3,13 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Post)
+# @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug',  'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
 
 
-
-
+admin.site.register(Post, PostAdmin)
