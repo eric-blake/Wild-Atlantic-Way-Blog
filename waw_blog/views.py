755410943ff5116ecc0ab707b84e89b370bf5415
eric-blake <>
2023-11-04 08:43:14 +0000

    install Allauth library

--- waw_blog/views.py
+++ waw_blog/views.py
@@ -28,5 +28,5 @@ class PostDetail(View):
             }
         )
 
-
+    
 
--- wildatlanticway/urls.py
+++ wildatlanticway/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('waw_blog.urls'), name='waw_blog-urls'),
     path('summernote/', include('django_summernote.urls')),
+    path('account/', include('allauth.urls')),
 ]
