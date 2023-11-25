b05818b5d103a1dd3e77813937cf5a128a1d6748
eric-blake <>
2023-11-25 20:42:38 +0000

    Update comment count and view count

--- waw_blog/views.py
+++ waw_blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.views import generic, View
 from .models import Post, Category
 from .forms import CommentForm
 from hitcount.views import HitCountDetailView, HitCountMixin
@@ -10,7 +10,6 @@ def blog(request):
     context = {}
     posts = Post.objects.filter(status=1).order_by("-hit_count_generic")
     categories = Category.objects.all()
-
     paginator = Paginator(posts, 4) 
     page_number = request.GET.get("page")
     posts = paginator.get_page(page_number)
@@ -27,18 +26,17 @@ def blog(request):
 class PostDetail(HitCountDetailView):
     model = Post
     count_hit = True
+
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
-        comment_count =post.comments.filter(approved=True).count()
+        comment_count = post.comments.filter(approved=True).count()
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-
-        return render(
-            request, 'post_detail.html',
-            {
+            
+        context = {
                 'post': post,
                 'comments': comments,
                 'commented': False,
@@ -46,7 +44,9 @@ class PostDetail(HitCountDetailView):
                 'comment_form': CommentForm(),
                 'comment_count': comment_count,
             }
-        )
+
+        return render(request, 'post_detail.html', context)
+        
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
@@ -67,16 +67,17 @@ class PostDetail(HitCountDetailView):
         else:
             comment_form = CommentForm()
 
-        return render(
-            request, 'post_detail.html',
-            {
+        context = {
                 'post': post,
                 'comments': comments,
-                'commented': True,
+                'commented': False,
                 'liked': liked,
-                'comment_form': CommentForm()
-            },
-        )
+                'comment_form': CommentForm(),
+            }
+
+        return render(request, 'post_detail.html', context)
+    
+
 
 
 class PostLike(View):
@@ -88,4 +89,4 @@ class PostLike(View):
         else:
             post.likes.add(request.user)
 
-            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
